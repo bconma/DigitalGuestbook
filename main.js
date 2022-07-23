8806fc5694fae63b70b3a9ae3d9ef1757ec546a9
nigelaclarke <nigel.a.clarke@gmail.com>
2022-07-23 15:53:52 -0700

    visual updates

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-var upload = new Upload({ apiKey: "public_12a1xnU6fR7pUFPnr9AJtSE7R37d" })
+var upload = new Upload({ apiKey: "public_12a1xnU6fR7pUFPnr9AJtSE7R37d" });
 var uploadFile = upload.createFileInputHandler({
+    onBegin: showThankyou,
     onUploaded: ({ fileUrl, fileId }) => {
-        showThankyou(fileUrl);
+        showPreview(fileUrl);
     }
 });
 
-function showThankyou(fileUrl) {
+function showThankyou() {
     document.getElementById("upload").className = "hidden";
+    document.getElementById("uploadingText").className = "";
     document.getElementById("thankyou").className = "";
+}
+
+function showPreview(fileUrl) {
     document.getElementById("uploadPreview").setAttribute("src", fileUrl);
+    document.getElementById("uploadingText").className = "hidden";
 }
